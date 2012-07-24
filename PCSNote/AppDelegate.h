d4276c2fe45251b10987a6ba8fa22711b5560df7
YM <tntlym@YMs-iMac.local>
2012-07-24 08:31:38 +0800

    Initial Commit

---
+++ PCSNote/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PCSNote
+//
+//  Created by Yongmo Liang on 7/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PCSNote/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PCSNote
+//
+//  Created by Yongmo Liang on 7/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
