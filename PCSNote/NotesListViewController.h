b95513cd7990bdb51a7edc35e68857bbdc577d1e
YM <tntlym@YMs-iMac.local>
2012-07-25 19:19:12 +0800

    start to work

---
+++ PCSNote/NotesListViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotesListViewController.h
+//  PCSNote
+//
+//  Created by Yongmo Liang on 7/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotesListViewController : UITableViewController
+
+@end
